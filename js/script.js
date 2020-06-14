3b06a8706b74b0c4ee502aea540fcb155b07b8a3
vandy1104 <vandana.chb@gmail.com>
2020-06-14 12:40:41 +1200

    Taste-of-India

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log('my script is working');
+
+$(document).ready(function(){
+
+	//Jquery checked
+// $('h1').click(function(){
+// 	$('h1').css('background', 'yellow');
+// });
+
+
+
+});
